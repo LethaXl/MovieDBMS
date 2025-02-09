5a45a94111e6211cf2224989cda9063937a47c79
LethaXl <154646038+LethaXl@users.noreply.github.com>
2025-02-09 00:49:23 -0500

    #1

--- general/connect.php
+++ general/connect.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 // Establish the connection
+
 $conn = oci_connect('****', '****', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
 
 if (!$conn) {
